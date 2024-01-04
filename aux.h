d0d701742bdf190f6f28c67874b124eb145b2719
thiagocalile <thiagoc@usp.br>
2024-01-04 19:18:52 -0300

    Documentado aux.h e aux.c

--- aux.h
+++ aux.h
@@ -5,6 +5,17 @@ typedef int Boolean;
 
 /**
  * Estrutura que define as informacoes basicas de cada palavra.
+ * 
+ * Elementos:
+ * 
+ * > char* palavra
+ *   A palavra que será guardada.
+ * 
+ * > int contagem
+ *   Quantidade de vezes que a palavra aparece.
+ * 
+ * > int* linha
+ *   Linhas em que a palavra aparece.
 */
 typedef struct tokenPalavra {
     char* palavra;
@@ -14,12 +25,27 @@ typedef struct tokenPalavra {
 
 /**
  * Estrutura que define o conteúdo de cada linha.
+ * 
+ * > char* conteudo
+ *   A string que vai do primeiro caracter até o \n.
+ * 
+ * > int tamanho
+ *   Número de tokens na linha.
 */
 typedef struct linha {
     char* conteudo;
     int tamanho;
 } Linha;
 
+/**
+ * O arquivo tokenizado.
+ * 
+ * >int numLinhas
+ *  Número de linhas no arquivo.
+ * 
+ * > Linha* linhas
+ *   Array das linhas do arquivo. 
+*/
 typedef struct arquivo {
     int numLinhas;
     Linha* linhas;
@@ -48,12 +74,12 @@ void toLowercase(char* palavra);
 void substituiChar(char* original, char substituir, char substituto);
 
 /**
- * Substitui todos os char pedidos.
+ * Substitui todos os char especiais pedidos, como $%#\().
 */
 void substituiTodoChar(char* original);
 
 /**
- * Insere uma nova Linha em um Arquivo.
+ * Insere uma nova struct Linha em um struct Arquivo.
 */
 void insereLinha(Arquivo * arquivo, char* linha);
 
@@ -63,13 +89,13 @@ void insereLinha(Arquivo * arquivo, char* linha);
 Arquivo * lerArquivo(FILE * file);
 
 /**
- * Funcao que atualiza um token adicionando todas as suas linhas em token.linhas.
+ * Funcao que atualiza um token adicionando todas as suas linhas no array token.linhas.
 */
 void updateToken(TokenPalavra* token, int linha);
 
 /**
  * Funcao que imprime um token e todas as suas informacoes. Deve ser usada quando
- * o token e encontrado na memoria,
+ * o token e encontrado na memoria.
 */
 void printToken(TokenPalavra* token, Arquivo* arquivo);
 
